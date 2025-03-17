c781bf39a7a7240e2da07d5152fc7530dadfa550
yaswanth-vempuluru <yaswanthvempuluru@outlook.com>
2025-03-17 15:12:10 +0530

    chore : implement clone trait

--- src/models/rune_pool.rs
+++ src/models/rune_pool.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, DisplayFromStr};
 
 #[serde_as]
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Meta {
     #[serde_as(as = "DisplayFromStr")]
     #[serde(rename = "startTime")]
@@ -25,7 +25,7 @@ pub struct Meta {
 }
 
 #[serde_as]
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Interval {
     #[serde_as(as = "DisplayFromStr")]
     #[serde(rename = "startTime")]
@@ -39,7 +39,7 @@ pub struct Interval {
     pub units: u64,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct RunePoolResponse {
     pub meta: Meta,
     pub intervals: Vec<Interval>,
